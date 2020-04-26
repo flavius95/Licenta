5e427da7cfa7e9a62eeb71bc316b824c00afdab9
Alex Albulescu <albulescu.victor.alexandru@gmail.com>
2020-04-26 19:09:43 +0300

    Add stopwords seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
       $this->call([
-        TopicsTableSeeder::class
+        TopicsTableSeeder::class,
+        StopWordsTableSeeder::class
       ]);
 
       $this->command->info('Tables seeded!');
